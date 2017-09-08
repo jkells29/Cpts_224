cc5030415d0b8b3bfc401fefde83d360e98c7e2d
jesse.kells@wsu.edu <jesse.kells@wsu.edu>
2017-09-07 20:01:13 -0700

    First commit unsucessful, trying another.

---
+++ intro.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Hello, let me introduce myself"
+
+NAME=Jesse
+HOMETOWN=Fairbanks
+MAJOR=CS
+FAVORITE_HOBBY=learn
+
+echo "My name is $NAME. I am originally from $HOMETOWN. My major is $MAJOR and in my free time I like to $FAVORITE_HOBBY"
